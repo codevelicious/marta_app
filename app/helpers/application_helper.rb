0ca5462a5be0d45b390fc191242ecb73ca858433
Yovel K. Johns <codevelicious@gmail.com>
2016-02-11 08:20:49 -0800

    First Commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+module ApplicationHelper
+
+		def nearby(userlong, userlat, buslong, buslat)
+			if (userlong - buslong).abs <= 0.01 && (userlat - buslat).abs <= 0.01
+				return true
+			else
+				return false
+		end
+
+	end
+
+end
